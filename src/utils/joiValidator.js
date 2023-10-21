0b65b6e54adbc0c340e420cc0d1524a607980c46
Sumitbansal-01 <uccsumitbansal@gmail.com>
2023-10-22 00:46:22 +0530

    it validate the incoming req

---
+++ src/utils/joiValidator.js
@@ -0,0 +1,17 @@
+function utils(schema, input) {
+    return async function (req, res, next) {
+        try {
+            const result = await schema.validate(req[input]);
+            if (result.error) {
+                return res.status(400).send({ error: 'Yes', value: result.error.toString() })
+            }else{
+                next()
+            }
+        } catch (err) {
+            console.log({ error: 'Yes', value: err.toString(), msg: 'in joi validation' })
+            return res.status(500).send({ error: 'Yes', value: err.toString() })
+        }
+    };
+}
+
+module.exports = utils
