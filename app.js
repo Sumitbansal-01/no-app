22d915a095a3761cd202abac7a1b70ec22becf79
Sumitbansal-01 <uccsumitbansal@gmail.com>
2023-10-22 09:54:03 +0530

    modified app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const {dotenv} = require("./src/config/importModulesConfig")
 dotenv()
 const app = require("./src/server")
 const port=process.env.PORT
+if(!port){
+    throw new Error("port is not present")
+}
 
 app.listen(port, async ()=>{
     console.log("\nApp is Listening on port",port)
