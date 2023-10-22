43f6fe8256952d16c0171a7b8ece19c705c3bbea
Sumitbansal-01 <uccsumitbansal@gmail.com>
2023-10-22 16:48:24 +0530

    minor modification in app.js file

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const {dotenv} = require("./src/config/importModulesConfig")
+const { dotenv } = require("./src/config/importModulesConfig")
 dotenv()
 const app = require("./src/server")
-const port=process.env.PORT
-if(!port){
-    throw new Error("port is not present")
+const port = process.env.PORT
+if (!port) {
+    throw new Error("port is not loaded from .env file")
 }
 
-app.listen(port, async ()=>{
-    console.log("\nApp is Listening on port",port)
+app.listen(port, async () => {
+    console.log("\nApp is Listening on port", port)
 })
+
+module.exports = app
